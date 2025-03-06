8b3b65f979c667a57d2cc1a4ae847b29dcbf47ad
karthik130487 <159273619+karthik130487@users.noreply.github.com>
2025-03-06 17:29:49 +0530

    link reset

--- BusinessNewEnvironment/Controllers/AuthController.cs
+++ BusinessNewEnvironment/Controllers/AuthController.cs
@@ -189,7 +189,7 @@ namespace Banking_Application.Controllers
         }
         private async Task SendResetEmail(string email, string token)
         {
-            string resetLink = $"https://github.com/Krishna1-karthik/BusinessApplication/Reset-password?token={token}";
+            string resetLink = $"https://krishna1-karthik.github.io/BusinessApplication/Reset-password?token={token}";
 
             string subject = "Password Reset Request";
             string body = $"Click the following link to reset your password: <a href='{resetLink}'>Reset Password</a>";
